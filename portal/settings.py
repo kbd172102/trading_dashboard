0ff8c12fabc0a089a6d8cc055bff2599e28bcc54
kbd172102 <kbd172102@gmail.com>
2026-01-04 00:14:33 +0530

    Configure SSL for database connection on Render

--- portal/settings.py
+++ portal/settings.py
@@ -94,13 +94,22 @@ WSGI_APPLICATION = 'portal.wsgi.application'
 
 DB_ENGINE = os.getenv("DB_ENGINE", "sqlite3")
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
-        conn_max_age=600,
-        conn_health_checks=True,
-    )
-}
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.config(
+            conn_max_age=600,
+            conn_health_checks=True,
+            ssl_require=True,
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 # if DB_ENGINE == "sqlite3":
 #     DATABASES = {
