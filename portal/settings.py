a82c90b1fb4744ceba9c03c983ebe66a9201efbf
kbd172102 <kbd172102@gmail.com>
2026-02-03 22:52:15 +0530

    update db

--- portal/settings.py
+++ portal/settings.py
@@ -18,7 +18,10 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR / ".env")
+# load_dotenv(BASE_DIR / ".env")
+if os.getenv("RENDER") is None:
+    load_dotenv(BASE_DIR / ".env")
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
